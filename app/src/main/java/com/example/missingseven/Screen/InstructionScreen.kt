fdec11fa702011d99a50c96c8d2071c24476545e
Xuhui Chen <xuhui.chen@mail.utoronto.ca>
2022-10-30 15:38:20 -0400

    add instruction screen

---
+++ app/src/main/java/com/example/missingseven/Screen/InstructionScreen.kt
@@ -0,0 +1,11 @@
+package com.example.missingseven.Screen
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun InstructionScreen(
+    instruction: String
+){
+    Text(text = instruction)
+}
