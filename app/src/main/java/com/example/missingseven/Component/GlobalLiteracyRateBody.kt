97de1d05d0881a8afcd082a39cfafd66b982e53f
Yuelin Jiang <yuelin.jiang@mail.utoronto.ca>
2022-12-07 20:57:26 -0500

    global literacy rate font change

--- app/src/main/java/com/example/missingseven/Component/GlobalLiteracyRateBody.kt
+++ app/src/main/java/com/example/missingseven/Component/GlobalLiteracyRateBody.kt
@@ -3,9 +3,11 @@ package com.example.missingseven.Component
 import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme.typography
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -15,6 +17,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.missingseven.Model.TaskUiState
 import com.example.missingseven.R
 import com.example.missingseven.Screen.DeepLinkText
@@ -27,15 +30,18 @@ fun GlobalLiteracyRateBody(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
             .fillMaxWidth()
+            .fillMaxHeight(0.7f)
+            .verticalScroll(rememberScrollState())
     ) {
         Text(
             text = task.content,
             modifier = Modifier.padding(top = 15.dp),
-            style = typography.h6,
+            fontSize = 15.sp
         )
 
         Image(
-            modifier = Modifier.padding(top = 15.dp)
+            modifier = Modifier
+                .padding(top = 15.dp)
                 .fillMaxWidth(),
             painter = painterResource(task.image),
             contentScale = ContentScale.Crop,
