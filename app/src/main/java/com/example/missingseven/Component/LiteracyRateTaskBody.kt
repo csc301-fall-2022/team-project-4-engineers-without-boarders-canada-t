184f964acf45d6b5fc2d0fe255ae5dfe662c0bcf
Ziyuan Gu <hardy.gu@mail.utoronto.ca>
2022-12-07 20:38:56 -0500

    Fix Multiple Choice

--- app/src/main/java/com/example/missingseven/Component/LiteracyRateTaskBody.kt
+++ app/src/main/java/com/example/missingseven/Component/LiteracyRateTaskBody.kt
@@ -1,13 +1,10 @@
 package com.example.missingseven.Component
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.ScrollState
-import androidx.compose.foundation.border
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyScopeMarker
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
@@ -18,9 +15,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Popup
 import com.example.missingseven.Model.TaskUiState
 import com.example.missingseven.R
 
@@ -31,12 +31,15 @@ fun LiteracyRateTaskBody(
     submitHandler: () -> Unit = {},
     task: TaskUiState.LiteracyRateTask
 ) {
-    var selected_country by remember {
+    var selectedCountry by remember {
         mutableStateOf("");
     }
 //    var Canada_selected: MutableState<Boolean> = mutableStateOf(false);
     val scrollState by remember {
         mutableStateOf(ScrollState(0))
+    }
+    var successPopup by remember {
+        mutableStateOf(false)
     }
     Column(
         modifier = Modifier
@@ -73,19 +76,19 @@ fun LiteracyRateTaskBody(
                 verticalAlignment = Alignment.CenterVertically,
             ) {
                 Button(
-                    onClick = {selected_country = "Canada"},
+                    onClick = {selectedCountry = "Canada"},
                     modifier = Modifier.padding(end = 4.dp)
                 ) {
                     Text(text = "Canada")
                 }
                 Button(
-                    onClick = {selected_country = "Germany"},
+                    onClick = {selectedCountry = "Germany"},
                     modifier = Modifier.padding(end = 4.dp)
                 ) {
                     Text(text = "Germany")
                 }
                 Button(
-                    onClick = {selected_country = "Ghana"},
+                    onClick = {selectedCountry = "Ghana"},
                     modifier = Modifier.padding(end = 4.dp)
                 ) {
                     Text(text = "Ghana")
@@ -93,19 +96,19 @@ fun LiteracyRateTaskBody(
             }
             Row() {
                 Button(
-                    onClick = {selected_country = "Kuwait"},
+                    onClick = {selectedCountry = "Kuwait"},
                     modifier = Modifier.padding(end = 4.dp)
                 ) {
                     Text(text = "Kuwait")
                 }
                 Button(
-                    onClick = {selected_country = "Malawi"},
+                    onClick = {selectedCountry = "Malawi"},
                     modifier = Modifier.padding(end = 4.dp)
                 ) {
                     Text(text = "Malawi")
                 }
                 Button(
-                    onClick = {selected_country = "Kenya"},
+                    onClick = {selectedCountry = "Kenya"},
                 ) {
                     Text(text = "Kenya")
                 }
@@ -114,46 +117,12 @@ fun LiteracyRateTaskBody(
                 horizontalArrangement = Arrangement.Center
             ) {
                 Button(
-                    onClick = {selected_country = "South Africa"},
+                    onClick = {selectedCountry = "South Africa"},
                 ) {
                     Text(text = "South Africa")
                 }
             }
 
-            when (selected_country){
-                "Canada" -> Text(
-                    text = "Literacy rate of Canada is: " + task.CanadaRate + "%",
-                    textAlign = TextAlign.Center
-                )
-                "Germany" -> Text(
-                    text = "Literacy rate of Germany is: " + task.GermanyRate + "%",
-                    textAlign = TextAlign.Center
-                )
-                "Ghana" -> Text(
-                    text = "Literacy rate of Ghana is: " + task.GhanaRate + "%",
-                    textAlign = TextAlign.Center
-                )
-                "Kenya" -> Text(
-                    text = "Literacy rate of Kenya is: " + task.KenyaRate + "%",
-                    textAlign = TextAlign.Center
-                )
-                "Kuwait" -> Text(
-                    text = "Literacy rate of Kuwait is: " + task.KuwaitRate + "%",
-                    textAlign = TextAlign.Center
-                )
-                "Malawi" -> Text(
-                    text = "Literacy rate of Malawi is: " + task.MalawiRate + "%",
-                    textAlign = TextAlign.Center
-                )
-                "South Africa" -> Text(
-                    text = "Literacy rate of South Africa is: " + task.SouthAfricaRate + "%",
-                    textAlign = TextAlign.Center
-                )
-                else -> Text(
-                    text = "",
-                    textAlign = TextAlign.Center
-                )
-            }
 
             Text(
                 text = "Which of these countries have the lowest rates?",
@@ -171,18 +140,75 @@ fun LiteracyRateTaskBody(
             Button(
                 modifier =  Modifier.padding(10.dp),
                 shape = RoundedCornerShape(50),
-                onClick = {submitHandler()}
+                onClick = {
+                    submitHandler()
+                    successPopup = true
+                }
             ) {
                 val text = "Submit"
                 Text(text = text)
             }
 
-            if(task.completed.value){
-                Text(
-                    text = task.successPopUp,
-                    textAlign = TextAlign.Center,
-                    modifier = Modifier.padding(top = 5.dp)
-                )
+        }
+        if (selectedCountry.isNotEmpty()){
+            Popup(
+                onDismissRequest = { selectedCountry = "" },
+                alignment = Alignment.Center
+            ) {
+                Box(
+                    modifier = Modifier
+                        .padding(24.dp)
+                        .background(Color.White)
+                        .border(
+                            0.dp,
+                            brush = SolidColor(Color.Black),
+                            shape = RoundedCornerShape(5.dp)
+                        )
+                ) {
+                    val text = when(selectedCountry){
+                        "Canada" -> "Literacy rate of Canada is: " + task.CanadaRate + "%"
+                        "Germany" -> "Literacy rate of Germany is: " + task.GermanyRate + "%"
+                        "Ghana" -> "Literacy rate of Ghana is: " + task.GhanaRate + "%"
+                        "Kenya" -> "Literacy rate of Kenya is: " + task.KenyaRate + "%"
+                        "Kuwait" -> "Literacy rate of Kuwait is: " + task.KuwaitRate + "%"
+                        "Malawi" -> "Literacy rate of Malawi is: " + task.MalawiRate + "%"
+                        "South Africa" -> "Literacy rate of South Africa is: " +
+                                task.SouthAfricaRate + "%"
+                        else -> ""
+                    }
+                    Text(
+                        text = text,
+                        textAlign = TextAlign.Center,
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.padding(8.dp)
+                    )
+                }
+            }
+        }
+        if (successPopup){
+            Popup(
+                onDismissRequest = { successPopup = false },
+                alignment = Alignment.Center
+            ) {
+                Box(
+                    modifier = Modifier
+                        .padding(24.dp)
+                        .background(Color.White)
+                        .border(
+                            0.dp,
+                            brush = SolidColor(Color.Black),
+                            shape = RoundedCornerShape(5.dp)
+                        )
+                ) {
+                    Text(
+                        text = if (task.completed.value) task.successPopUp else "Wrong Answer",
+                        textAlign = TextAlign.Center,
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.padding(8.dp)
+                    )
+                }
             }
         }
     }
