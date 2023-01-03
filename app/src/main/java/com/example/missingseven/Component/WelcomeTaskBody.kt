66bf364cb87b9b58c116e75f1594fd8dda58ea3b
Xuhui Chen <xuhui.chen@mail.utoronto.ca>
2023-01-02 22:27:26 -0500

    add gif to welcome page

--- app/src/main/java/com/example/missingseven/Component/WelcomeTaskBody.kt
+++ app/src/main/java/com/example/missingseven/Component/WelcomeTaskBody.kt
@@ -1,6 +1,9 @@
 package com.example.missingseven.Component
 
+import android.os.Build.VERSION.SDK_INT
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material.Button
@@ -9,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
@@ -18,6 +22,13 @@ import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.ImageLoader
+import coil.compose.rememberAsyncImagePainter
+import coil.decode.GifDecoder
+import coil.decode.ImageDecoderDecoder
+import coil.request.ImageRequest
+import coil.size.Size
+import com.example.missingseven.R
 import com.example.missingseven.ViewModel.TaskViewModel
 
 /***
@@ -42,7 +53,7 @@ fun WelcomeTaskBody(
                     " please complete the ",
             annotated = "LOGIN",
             after = " screen",
-            modifier = Modifier.padding(top = 100.dp)) {
+            modifier = Modifier.padding(top = 40.dp)) {
             viewModel.loginClicked()
         }
         Button(
@@ -50,6 +61,9 @@ fun WelcomeTaskBody(
             onClick = { viewModel.skip() }) {
             Text(text = "Skip to water filter exercise")
         }
+        GifImage(
+            modifier = Modifier.padding(top = 10.dp)
+        )
     }
 }
 
@@ -97,5 +111,30 @@ fun AnnotatedClickableText(
                     onClick()
                 }
         }
+    )
+}
+
+@Composable
+fun GifImage(
+    modifier: Modifier = Modifier,
+) {
+    val context = LocalContext.current
+    val imageLoader = ImageLoader.Builder(context)
+        .components {
+            if (SDK_INT >= 28) {
+                add(ImageDecoderDecoder.Factory())
+            } else {
+                add(GifDecoder.Factory())
+            }
+        }
+        .build()
+    Image(
+        painter = rememberAsyncImagePainter(
+            ImageRequest.Builder(context).data(data = R.drawable.tap_water).apply(block = {
+                size(Size.ORIGINAL)
+            }).build(), imageLoader = imageLoader
+        ),
+        contentDescription = null,
+        modifier = modifier.fillMaxWidth(),
     )
 }
