bb72999a67829440818ef4098b1b801f58f12924
Xuhui Chen <xuhui.chen@mail.utoronto.ca>
2023-01-02 22:21:48 -0500

    add keyboard done action for short answer questions

--- app/src/main/java/com/example/missingseven/Component/ShortAnswerTaskBody.kt
+++ app/src/main/java/com/example/missingseven/Component/ShortAnswerTaskBody.kt
@@ -3,19 +3,25 @@ package com.example.missingseven.Component
 import android.content.Context
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Button
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import com.example.missingseven.Model.TaskUiState
 
 /***
  * composable function for the short answer screen
  */
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun ShortAnswerTaskBody(
     saveHandler: () -> Unit,
@@ -30,6 +36,8 @@ fun ShortAnswerTaskBody(
             .padding(top = 10.dp)
     ) {
 
+        val keyboardController = LocalSoftwareKeyboardController.current
+
         OutlinedTextField(
             value = task.answer.value,
             label = { Text(text = "Enter Your Answer") },
@@ -39,7 +47,14 @@ fun ShortAnswerTaskBody(
             modifier = Modifier
                 .height(250.dp)
                 .padding(horizontal = 5.dp, vertical = 20.dp)
-                .fillMaxWidth()
+                .fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+            keyboardActions = KeyboardActions(
+                onDone = {
+                    saveHandler()
+                    keyboardController?.hide()
+                }
+            )
         )
 
         Button(
